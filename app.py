c3b71e181e09b7c150ef9d09281a878e94ade678
Chris <clj@ollure.com>
2019-06-05 14:36:40 -0500

    building database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import * 
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app = Flask(__name__)
 
@@ -13,9 +16,18 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-	if request.form['password'] == 'password' and request.form['username'] == 'admin': session['logged_in'] = True
+
+	POST_USERNAME = str(request.form['username'])
+	POST_PASSWORD = str(request.form['password'])
+
+	Session = sessionmaker(bind=engine)
+	s = Session()
+	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+	result = query.first()
+	if result:
+		session['logged_in'] = True
 	else:
-		flash('Wrong password!')
+		flash('Wrong password!, try again.')
 	return home()
 
 @app.route("/logout")
@@ -26,3 +38,19 @@ def logout():
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
 	app.run(debug=True,host='0.0.0.0', port=5000)
+
+@app.route('/test')
+def test():
+
+	POST_USERNAME = 'python'
+	POST_PASSWORD = 'python'
+
+	Session = sessionmaker(bind=engine)
+	s = Session()
+	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+	result = query.first()
+	if result:
+		return "Object found"
+	else:
+		return "Object not found" + POST_USERNAME + " " + POST_PASSWORD
+
